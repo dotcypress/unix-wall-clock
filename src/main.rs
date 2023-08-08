7660c963ce63ddcf947926f2254540d49015e893
Vitaly Domnikov <dotcypress@gmail.com>
2023-08-07 20:58:12 -0700

    gps control

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ extern crate rtic;
 extern crate stm32g0xx_hal as hal;
 
 mod display;
-mod remote;
 mod watch;
 
 use defmt_rtt as _;
 
 use hal::gpio::*;
 use hal::prelude::*;
+use hal::serial::*;
 use hal::spi::*;
 use hal::stm32;
 use hal::timer::*;
@@ -25,7 +25,7 @@ use watch::*;
 pub type Ir = PeriodicReceiver<Nec, gpioc::PC15<Input<Floating>>>;
 pub type Display = display::DisplayController<
     Spi<stm32::SPI2, (gpiob::PB8<Analog>, NoMiso, gpioa::PA4<Analog>)>,
-    gpioa::PA3<Output<PushPull>>,
+    gpioa::PA6<Output<PushPull>>,
 >;
 
 #[rtic::app(device = stm32, peripherals = true)]
@@ -36,8 +36,8 @@ mod app {
     struct Local {
         ir: Ir,
         animation_timer: Timer<stm32::TIM14>,
-        clock_timer: Timer<stm32::TIM17>,
         ir_timer: Timer<stm32::TIM16>,
+        uart: Serial<stm32::USART2, BasicConfig>,
     }
 
     #[shared]
@@ -54,12 +54,8 @@ mod app {
         ir_timer.start(20.khz());
         ir_timer.listen();
 
-        let mut clock_timer = ctx.device.TIM17.timer(&mut rcc);
-        clock_timer.start(1.hz());
-        clock_timer.listen();
-
         let mut animation_timer = ctx.device.TIM14.timer(&mut rcc);
-        animation_timer.start(8.hz());
+        animation_timer.start(20.hz());
         animation_timer.listen();
 
         let port_a = ctx.device.GPIOA.split(&mut rcc);
@@ -73,24 +69,86 @@ mod app {
             &mut rcc,
         );
         port_a.pa5.into_push_pull_output().set_low().unwrap();
-        let mut display = Display::new(spi, port_a.pa3.into());
+        let mut display = Display::new(spi, port_a.pa6.into());
 
-        let watch = Watch::new();
+        let mut watch = Watch::new();
         watch.animate(&mut display);
         display.render();
 
         let ir = PeriodicReceiver::new(port_c.pc15.into(), 20_000);
 
+        let uart_cfg = BasicConfig::default().baudrate(9600.bps());
+        let mut uart = ctx
+            .device
+            .USART2
+            .usart(port_a.pa2, port_a.pa3, uart_cfg, &mut rcc)
+            .expect("Failed to init serial port");
+        uart.listen(Event::Rxne);
+
+        defmt::info!("let's go!");
         (
             Shared { display, watch },
             Local {
                 ir,
                 animation_timer,
                 ir_timer,
-                clock_timer,
+                uart,
             },
             init::Monotonics(),
         )
+    }
+
+    #[task(
+        binds = USART2, 
+        local = [
+            uart,
+            needle: usize = 0,
+            scratch: [u8; 255] = [0; 255],
+        ],
+        shared = [watch])]
+    fn uart_rx(mut ctx: uart_rx::Context) {
+        loop {
+            match ctx.local.uart.read() {
+                Err(hal::nb::Error::WouldBlock) => return,
+                Err(hal::nb::Error::Other(_)) => {
+                    defmt::error!("UART RX ERROR");
+                }
+                Ok(b'\n') => {
+                    if let Ok(line) = core::str::from_utf8(&ctx.local.scratch[..*ctx.local.needle]) {
+                        if line.contains("ANTENNA OPEN") { 
+                            defmt::info!("ANTENNA OPEN");
+                        } else if line.contains("ANTENNA OK") {
+                            defmt::info!("ANTENNA OK");
+                        } else if line.starts_with("$GNZDA") {
+                            fn parse(s: Option<&str>) -> u64 {
+                                s.unwrap_or("").parse::<u64>().unwrap_or(0)
+                            }
+                            // $GNZDA,004101.000,08,08,2023,00,00*4F
+                            let mut chunks = line.split(',').skip(1);
+                            let time = parse(
+                                chunks
+                                    .next()
+                                    .unwrap_or("")
+                                    .split('.')
+                                    .next()
+                            );
+                            let seconds = time % 100;
+                            let minutes = (time / 100) % 100;
+                            let hours =  (time / 10_000) % 100;
+                            let day = parse(chunks.next());
+                            let month = parse(chunks.next());
+                            let year = parse(chunks.next());
+                            ctx.shared.watch.lock(|watch| watch.set_utc_time(year, month, day, hours, minutes, seconds));
+                        }
+                    }
+                    *ctx.local.needle = 0;
+                }
+                Ok(b) => {
+                    ctx.local.scratch[*ctx.local.needle] = b;
+                    *ctx.local.needle += 1;
+                }
+            }
+        }
     }
 
     #[task(binds = TIM16, local = [ir, ir_timer], shared = [watch])]
@@ -99,12 +157,6 @@ mod app {
             ctx.shared.watch.lock(|watch| watch.ir_command(cmd));
         }
         ctx.local.ir_timer.clear_irq();
-    }
-
-    #[task(binds = TIM17, local = [clock_timer], shared = [watch])]
-    fn tick(mut ctx: tick::Context) {
-        ctx.shared.watch.lock(|watch| watch.tick());
-        ctx.local.clock_timer.clear_irq();
     }
 
     #[task(binds = TIM14, priority = 2, local = [animation_timer], shared = [display, watch])]
--- src/watch.rs
+++ src/watch.rs
@@ -1,7 +1,5 @@
 use infrared::protocols::nec::NecCommand;
 
-use crate::remote;
-
 #[allow(dead_code)]
 enum Mode {
     UnixClock,
@@ -9,6 +7,7 @@ enum Mode {
 }
 pub struct Watch {
     mode: Mode,
+    frame: usize,
     brightness: u8,
     ts: u64,
 }
@@ -18,31 +17,41 @@ impl Watch {
         Self {
             brightness: 4,
             mode: Mode::UnixClock,
-            ts: 1_654_839_000_000,
+            ts: 0,
+            frame: 0,
         }
     }
 
-    pub fn tick(&mut self) {
-        self.ts += 1_000;
+    pub fn set_utc_time(
+        &mut self,
+        year: u64,
+        month: u64,
+        day: u64,
+        hour: u64,
+        minutes: u64,
+        seconds: u64,
+    ) {
+        let month_yday: [u64; 12] = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+        let year_adj = year + 4800;
+        let febs = year_adj - (if month <= 2 { 1 } else { 0 });
+        let leap_days = 1 + (febs / 4) - (febs / 100) + (febs / 400);
+        let days = 365 * year_adj + leap_days + month_yday[month as usize - 1] + day - 1 - 2472692;
+        self.ts = days * 24 * 60 * 60 + hour * 60 * 60 + minutes * 60 + seconds;
+        self.frame = 0;
     }
 
     pub fn ir_command(&mut self, cmd: NecCommand) {
         match cmd.cmd {
-            remote::A => self.mode = Mode::UnixClock,
-            remote::B => self.mode = Mode::DoomsdayClock,
-            remote::UP => self.ts = self.ts.saturating_add(1_000),
-            remote::DOWN => self.ts = self.ts.saturating_sub(1_000),
-            remote::RIGTH => self.ts = self.ts.saturating_add(10_000),
-            remote::LEFT => self.ts = self.ts.saturating_sub(10_000),
-            remote::PLUS => self.brightness = self.brightness.saturating_add(8),
-            remote::MINUS => self.brightness = self.brightness.saturating_sub(8),
+            7 | 70 => self.mode = Mode::DoomsdayClock,
+            71 | 69 => self.mode = Mode::UnixClock,
+            67 => self.brightness = self.brightness.saturating_add(8),
+            64 | 13 => self.brightness = self.brightness.saturating_sub(8),
+            68 | 21 => self.brightness = 16,
             _ => {}
         }
-        // (Mode::UnixClock, Button::Up) | (Mode::DoomsdayClock, Button::Up) => self.ts += 250,
-        // (Mode::UnixClock, Button::Down) | (Mode::DoomsdayClock, Button::Down) => self.ts -= 250,
     }
 
-    pub fn animate(&self, display: &mut crate::Display) {
+    pub fn animate(&mut self, display: &mut crate::Display) {
         const DIGITS: [u8; 10] = [
             0b0111111, 0b0000110, 0b1011011, 0b1001111, 0b1100110, 0b1101101, 0b1111101, 0b0000111,
             0b1111111, 0b1101111,
@@ -50,17 +59,25 @@ impl Watch {
 
         match self.mode {
             Mode::DoomsdayClock | Mode::UnixClock => {
-                let mut num = match self.mode {
-                    Mode::UnixClock => self.ts / 1_000,
-                    Mode::DoomsdayClock => i32::MAX as u64 - self.ts / 1_000,
+                let mut val = match self.mode {
+                    Mode::UnixClock => self.ts,
+                    Mode::DoomsdayClock => i32::MAX as u64 - self.ts,
                 };
-
                 for pos in 0..10 {
-                    let digit = num % 10;
-                    display.print(9 - pos, DIGITS[digit as usize], self.brightness);
-                    num = num / 10;
+                    let brightness = match self.mode {
+                        Mode::UnixClock => self.brightness,
+                        Mode::DoomsdayClock => {
+                            let half = self.brightness as usize;
+                            let brightness = half * ((self.frame + pos) % 20) / 10;
+                            brightness as u8
+                        }
+                    };
+                    let digit = val % 10;
+                    display.print(9 - pos, DIGITS[digit as usize], brightness);
+                    val = val / 10;
                 }
             }
         }
+        self.frame += 1;
     }
 }
